b730308fc440f515845cce020c4f5508cc545f23
BunjiKugashira <alex_otto@cityweb.de>
2021-11-18 18:15:26 +0100

    Add Collections Extensions Project

---
+++ CollectionsExtensions/EnumerableSequenceEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace CollectionsExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EnumerableSequenceEqualityComparer<T> : IEqualityComparer<IEnumerable<T>>
+    {
+        private readonly IEqualityComparer<T> itemComparer;
+
+        public EnumerableSequenceEqualityComparer(IEqualityComparer<T> itemComparer = null)
+        {
+            this.itemComparer = itemComparer;
+        }
+
+        public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+        {
+            return x == null || y == null
+                ? x == null && y == null
+                : x.SequenceEqual(y, this.itemComparer);
+        }
+
+        public int GetHashCode(IEnumerable<T> obj)
+        {
+            return obj?.Aggregate(0, (aggr, item) => HashCode.Combine(aggr, this.itemComparer?.GetHashCode(item) ?? item?.GetHashCode() ?? 0)) ?? 0;
+        }
+    }
+}
---
+++ CollectionsExtensions/LambdaEqualityComparer.cs
@@ -0,0 +1,32 @@
+﻿namespace CollectionsExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    public class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        protected readonly Func<T, T, bool> equalityLambda;
+        protected readonly Func<T, int> hashFunction;
+
+        public LambdaEqualityComparer([DisallowNull] Func<T, T, bool> comparisonFunction, [DisallowNull] Func<T, int> hashFunction)
+        {
+            this.equalityLambda = comparisonFunction;
+            this.hashFunction = hashFunction;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return x == null || y == null
+                ? x == null && y == null
+                : this.equalityLambda(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj == null
+                ? 0
+                : this.hashFunction(obj);
+        }
+    }
+}
